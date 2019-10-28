5b18ba413e7ea8f8cf30ef3117180806b8afb2ac
Kimio Kuramitsu <kkuramitsu@gmail.com>
2019-10-28 19:50:07 +0900

    fix /api/option

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/nlp/<string:NLPSymbol>', methods=["POST"])
+
+@app.route('/api/option/<string:NLPSymbol>', methods=["POST", "GET"])
 def nlp(NLPSymbol):
-  return jsonify(conv2(NLPSymbol))
+    print(NLPSymbol)  # デバック用
+    return jsonify(conv2(NLPSymbol))
+
 
 def main():
-  port = int(os.environ.get("PORT", 8888))
-  app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 8888))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 if __name__ == '__main__':
-  main()
+    main()
